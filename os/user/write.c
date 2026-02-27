d8d4b6d8c352304d3dbf3d1c1274e30da409739d
kk <72641015+ling000ling000@users.noreply.github.com>
2026-02-27 18:41:49 +0800

    multitask based on address space

--- os/user/write.c
+++ os/user/write.c
@@ -1,34 +1,34 @@
 #include "../include/os.h"
 #include "../include/stdio.h"
 
-uint64_t syscall(size_t id, reg_t arg1, reg_t arg2, reg_t arg3)
-{
-    register uintptr_t a0 asm ("a0") = (uintptr_t)(arg1);
-    register uintptr_t a1 asm ("a1") = (uintptr_t)(arg2);
-    register uintptr_t a2 asm ("a2") = (uintptr_t)(arg3);
-    register uintptr_t a7 asm ("a7") = (uintptr_t)(id);
+// uint64_t syscall(size_t id, reg_t arg1, reg_t arg2, reg_t arg3)
+// {
+//     register uintptr_t a0 asm ("a0") = (uintptr_t)(arg1);
+//     register uintptr_t a1 asm ("a1") = (uintptr_t)(arg2);
+//     register uintptr_t a2 asm ("a2") = (uintptr_t)(arg3);
+//     register uintptr_t a7 asm ("a7") = (uintptr_t)(id);
 
-    asm volatile ("ecall"
-              : "+r" (a0)
-              : "r" (a1), "r" (a2), "r" (a7)
-              : "memory");
-    return a0;
-}
+//     asm volatile ("ecall"
+//               : "+r" (a0)
+//               : "r" (a1), "r" (a2), "r" (a7)
+//               : "memory");
+//     return a0;
+// }
 
+// // uint64_t sys_write(size_t fd, const char* buf, size_t len)
+// // {
+// //     syscall(__NR_write, fd, buf, len);
+// // }
 // uint64_t sys_write(size_t fd, const char* buf, size_t len)
 // {
-//     syscall(__NR_write, fd, buf, len);
+//     return syscall(__NR_write, (reg_t)fd, (reg_t)(uintptr_t)buf, (reg_t)len);
 // }
-uint64_t sys_write(size_t fd, const char* buf, size_t len)
-{
-    return syscall(__NR_write, (reg_t)fd, (reg_t)(uintptr_t)buf, (reg_t)len);
-}
 
 
 int main()
 {
-    const char *message = "task1 is running!\n";
-    int len = strlen(message);
+    const char *message = "task write is running!\n";
+    // int len = strlen(message);
     while (1)
     {
         printf(message);
